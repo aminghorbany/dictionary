c51602b40ad21b5100a0921221441e4cd697d568
Amin <z@z.com>
2024-06-17 11:08:58 +0330

    feat : add source to github.

---
+++ app/src/main/java/com/example/dictionary/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.dictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dictionary.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.apply {
+            txtName.text = "haj amin ghorbany"
+        }
+    }
+}
