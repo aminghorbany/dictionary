eeca7e2022f36128f6fc85f030f6703816333af2
Amin <z@z.com>
2024-06-18 01:41:21 +0330

    feat : info design done.

--- app/src/main/java/com/example/dictionary/ui/info/InfoFragment.kt
+++ app/src/main/java/com/example/dictionary/ui/info/InfoFragment.kt
@@ -1,24 +1,20 @@
 package com.example.dictionary.ui.info
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.dictionary.databinding.FragmentFavoriteBinding
 import com.example.dictionary.databinding.FragmentInfoBinding
+import com.example.dictionary.utils.showLongSnackBar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class InfoFragment : Fragment() {
-
     private lateinit var binding: FragmentInfoBinding
-//    @Inject
-//    lateinit var favoritesAdapter: FavoritesAdapter
-//    private val viewModel: FavoriteViewModel by viewModels()
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-
         binding = FragmentInfoBinding.inflate(layoutInflater)
         return binding.root
     }
@@ -26,26 +22,44 @@ class InfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-//            viewModel.getAllFavoriteListMovies() // its look like call api
-//            viewModel.allFavoriteListMoviesLiveData.observe(viewLifecycleOwner) {
-//                favoritesAdapter.setData(it)
-//                favoriteRecycler.initRecyclerView(LinearLayoutManager(requireContext()), favoritesAdapter)
-//            }
-//            viewModel.empty.observe(viewLifecycleOwner) {
-//                if (it) {
-//                    requireContext().showWidget(emptyItemsLay)
-//                    requireContext().goneWidget(favoriteRecycler)
-//                } else {
-//                    requireContext().showWidget(favoriteRecycler)
-//                    requireContext().goneWidget(emptyItemsLay)
-//                }
-//            }
-            //onItemClick
-//            favoritesAdapter.onItemClickListener {
-//                val direction = FavoriteFragmentDirections.actionToDetailFragment(it.id) // FavoriteFragmentDirections origin
-//                findNavController().navigate(direction)
-//            }
+            imageViewGithub.setOnClickListener {
+                openGithub()
+            }
+            txtGithub.setOnClickListener {
+                openGithub()
+            }
+            imageViewContact.setOnClickListener {
+                openCallScreen()
+            }
+            txtPhoneNumber.setOnClickListener {
+                openCallScreen()
+            }
+            imageViewEmail.setOnClickListener {
+                openEmailApp()
+            }
+            txtEmail.setOnClickListener {
+                openEmailApp()
+            }
         }
     }
 
+    private fun openGithub(){
+        val url = "https://github.com/aminghorbany"
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
+    }
+
+    private fun openCallScreen(){
+        val url = "+989037052133"
+        val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel" , url , null))
+        startActivity(intent)
+    }
+
+    private fun openEmailApp() {
+        val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                "mailto", "m.a.ghorbany73@gmail.com" , null
+            )
+        )
+        startActivity(Intent.createChooser(emailIntent, ""))
+    }
 }
