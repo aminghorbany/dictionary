e4c14267711a2b74ff49423211ac9a3b0b7c1cf6
Amin <z@z.com>
2024-06-18 22:26:57 +0330

    feat : debounce added to searchEdt.

--- app/src/main/java/com/example/dictionary/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/dictionary/ui/search/SearchFragment.kt
@@ -1,18 +1,24 @@
 package com.example.dictionary.ui.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dictionary.databinding.FragmentSearchBinding
 import com.example.dictionary.utils.goneWidget
 import com.example.dictionary.utils.showWidget
 import com.example.dictionary.viewmodel.SearchViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -21,6 +27,8 @@ class SearchFragment : Fragment() {
     private lateinit var binding: FragmentSearchBinding
     @Inject lateinit var searchAdapter : SearchAdapter
     private val viewModel : SearchViewModel by viewModels()
+    private var searchJob: Job? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSearchBinding.inflate(layoutInflater)
         return binding.root
@@ -29,11 +37,23 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.apply {
-            searchEdt.addTextChangedListener {
-                if (it.toString().isNotEmpty()){
-                    viewModel.getFilteredWords(it.toString())
+            searchEdt.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    searchJob?.cancel()
                 }
-            }
+
+                override fun afterTextChanged(s: Editable?) {
+                    s?.let {
+                        searchJob = lifecycleScope.launch {
+                            delay(1000)
+                            if (it.toString().isNotEmpty()) {
+                                viewModel.getFilteredWords(it.toString())
+                            }
+                        }
+                    }
+                }
+            })
 
             //observe
             viewModel.dictionaryEntityLiveData.observe(viewLifecycleOwner){
